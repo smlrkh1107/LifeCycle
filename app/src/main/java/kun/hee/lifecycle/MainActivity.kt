24e4d545d3535c5071c9c466c615dbe85b6de1ec
smlrkh1107 <krae7789230@gmail.com>
2020-04-16 21:16:52 +0900

    생명주기 기초 탐구

--- app/src/main/java/kun/hee/lifecycle/MainActivity.kt
+++ app/src/main/java/kun/hee/lifecycle/MainActivity.kt
@@ -7,6 +7,24 @@ import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
+        Log.d("MainActivity","onCreate실행됨")
+        // 로그가 한번만 실행됨. 다른곳 다녀왔다고해서 다시 로그 찍히지 않아.
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("MainActivity", "onResume실행됨")
+//        해당화면으로 return하면 다시 로그 찍히지. 다른화면 갔다오면 다시불러옴.
+//        게시글을 쓰고 다시 돌아왔을 때, 새로운 게시글도 다시 불러오게 해준다.
+    }
+
     override fun setupEvents() {
         nextBtn.setOnClickListener {
             val myIntent = Intent(mContext,NextActivity::class.java)
@@ -18,11 +36,5 @@ class MainActivity : BaseActivity() {
 
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setupEvents()
-        setValues()
-        Log.d("MainActivity","onCreate실행됨")
-    }
+
 }
