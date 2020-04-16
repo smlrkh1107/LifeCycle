d1f645e779e783a070bde52a0844305e8277b177
smlrkh1107 <krae7789230@gmail.com>
2020-04-16 21:30:54 +0900

    생명주기 기초 탐구2 [화면 별 Create/Resume 로그보면서 차이점 알기]

--- app/src/main/java/kun/hee/lifecycle/NextActivity.kt
+++ app/src/main/java/kun/hee/lifecycle/NextActivity.kt
@@ -2,9 +2,27 @@ package kun.hee.lifecycle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class NextActivity :BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_next)
+        setupEvents()
+        setValues()
+
+        Log.d("NextActivity","onCreate실행됨")
+//       Main 화면에서 버튼 눌러질때마다 Create된다~!
+//        버튼 누르기 전에는 Oncreate되지 않아~!
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("NextActivity","onResume실행됨")
+    }
+
     override fun setupEvents() {
 
     }
@@ -13,10 +31,4 @@ class NextActivity :BaseActivity() {
 
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_next)
-        setupEvents()
-        setValues()
-    }
 }
